80812ff1684d8941311a0717ac2ad19dfc007538
pwshady <pwshady@gmail.com>
2022-04-02 17:13:10 +0300

    0204-1

--- src/main/kotlin/pars.kt
+++ src/main/kotlin/pars.kt
@@ -1,6 +1,14 @@
 import Vievs.ParserApp
+import org.jsoup.Jsoup
 import tornadofx.launch
+import java.io.File
 
 fun main(){
-        launch<ParserApp>()
+    val file = File("C:\\Users\\4\\Desktop\\test01.html")
+    var doc = Jsoup.parse(file, "UTF-8", "C:\\Users\\4\\Desktop\\test01.html")
+    println(doc.children()[0].children().size)
+    for (el in doc.children()[0].children()){
+        println(el.children().size)
     }
+    launch<ParserApp>()
+}
